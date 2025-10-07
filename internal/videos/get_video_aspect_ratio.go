601dfb2d76627d246814a1d987f10f8837fe45c9
abdo-355 <abdoluxor366@gmail.com>
2025-10-07 12:39:54 +0300

    add an internal helper fuction to get the aspect ratio of the video

---
+++ internal/videos/get_video_aspect_ratio.go
@@ -0,0 +1,55 @@
+// Package videos handles different operations on videos
+package videos
+
+import (
+	"bytes"
+	"encoding/json"
+	"math"
+	"os/exec"
+)
+
+const Tolerance = 0.01
+
+// GetVideoAspectRatio returns the aspect ratio of a video in the provided filePath
+// the returned aspect ratio is in a string format along wiht an error
+func GetVideoAspectRatio(filePath string) (string, error) {
+	// ffprobe command to show the streams date of the video
+	// which is going to be used to get the width and height of the video
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+
+	stdOutBuff := bytes.Buffer{}
+
+	cmd.Stdout = &stdOutBuff
+
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	var data struct {
+		Streams []struct {
+			Height int `json:"height"`
+			Width  int `json:"width"`
+		} `json:"streams"`
+	}
+
+	err = json.Unmarshal(stdOutBuff.Bytes(), &data)
+	if err != nil {
+		return "", err
+	}
+
+	ratio := float64(data.Streams[0].Width) / float64(data.Streams[0].Height)
+
+	targets := map[string]float64{
+		"16:9": 16.0 / 9.0,
+		"9:16": 9.0 / 16.0,
+	}
+
+	for label, target := range targets {
+		if math.Abs(ratio-target) <= Tolerance {
+			return label, nil
+		}
+	}
+
+	return "other", nil
+}
