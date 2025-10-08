370a4b697d6747db0c372edcbcb8046b0cabcc7e
abdo-355 <abdoluxor366@gmail.com>
2025-10-08 10:29:23 +0300

    add a helper fuction to move the moov atom to the start of the video file

---
+++ internal/videos/process_video_for_fast_start.go
@@ -0,0 +1,16 @@
+package videos
+
+import "os/exec"
+
+func ProcessVideoForFastStart(filePath string) (string, error) {
+	outputPath := filePath + ".processing"
+
+	cmd := exec.Command("ffmpeg", "-i", filePath, "-c", "copy", "-movflags", "faststart", "-f", "mp4", outputPath)
+
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return outputPath, nil
+}
